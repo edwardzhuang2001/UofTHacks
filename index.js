34c7a5c4900b910fc01d40a30adf0ef1c0cd346a
Labeeb Hasan Zaker <labeebzaker@gmail.com>
2020-01-17 20:10:26 -0800

    modified index.ejs

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var express = require("express")
     app = express()
 
+app.set("view engine", "ejs");
+
+app.get("/",function(req, res){
+  res.render("index")
+})
 app.listen(3000,function(req, res){
   console.log("Hello world")
 })

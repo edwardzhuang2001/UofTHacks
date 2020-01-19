71e4ffbe1bdc29cd0fc56c137cbd5d2de84a080c
Labeeb Hasan Zaker <labeebzaker@gmail.com>
2020-01-18 21:02:21 -0800

    added courses tab to view courses

---
+++ models/course.js
@@ -0,0 +1,13 @@
+var mongoose = require("mongoose")
+var courseSchema = new mongoose.Schema({
+  title: String,
+  prof_name: String,
+  course_code: String,
+  reviews: [
+    {
+       type: mongoose.Schema.Types.ObjectId,
+       ref: "Review"
+    }
+]
+})
+module.exports = mongoose.model("Course",courseSchema)
